37b287ce1ad60a44c89effbc8fef26ea975a5745
Shoaib-Khalid-11 <shoaibkhalid3300@gmail.com>
2026-05-21 19:43:59 +0500

    update user create api update for user

--- Backend/src/controllers/user.controller.ts
+++ Backend/src/controllers/user.controller.ts
@@ -31,3 +31,45 @@ export const getUserByID = catchAsyncErrors(async (req, res, next) => {
     next(new ErrorHandler("Server error", 500));
   }
 });
+export const updateUser = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { name, email, role, password, ...customFields } = req.body;
+
+    const queryId =
+      id?.length === 24 ? new mongoose.Types.ObjectId(id as string) : id;
+    const collection = mongoose.connection.collection("user");
+    const existingUser = await collection.findOne({
+      _id: queryId as mongoose.Types.ObjectId,
+    });
+    if (!existingUser) {
+      return next(new ErrorHandler("User not found", 404));
+    }
+    const updatedPayload = {
+      name,
+      email,
+      role,
+      ...customFields,
+    };
+    // Remove undefine/null keys from the update payload
+    Object.keys(updatedPayload).forEach((key) => {
+      (updatedPayload[key] === undefined || updatedPayload[key] === null) &&
+        delete updatedPayload[key];
+    });
+    const result = await collection.updateOne(
+      { _id: new mongoose.Types.ObjectId(id as string) },
+      {
+        $set: updatedPayload,
+      },
+    );
+    if (result.matchedCount === 0) {
+      return next(new ErrorHandler("User not found", 404));
+    }
+    res.json({
+      message: "User updated successfully",
+      updateUser: result,
+    });
+  } catch (error) {
+    next(new ErrorHandler("Server error", 500));
+  }
+});
--- Backend/src/routers/user.router.ts
+++ Backend/src/routers/user.router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getUserByID } from "../controllers/user.controller";
+import { getUserByID, updateUser } from "../controllers/user.controller";
 import { requireAuth } from "../middlewares/auth";
 
 const userRouter = Router();
 
 userRouter.get("/:id", requireAuth, getUserByID);
+userRouter.put("/update/:id", requireAuth, updateUser);
 
 export default userRouter;
